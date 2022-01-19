34c6d0b44e79c96fef571b04708ec009c5cbb952
flintdk <92164637+flintdk@users.noreply.github.com>
2022-01-19 20:52:15 +0000

    Test main class

---
+++ main.py
@@ -0,0 +1,33 @@
+# 2022-01-19 TK; Learning Python 3 
+"""
+PseudoCode Solution:
+
+define a function which
+    return 
+
+define a main function
+    print finished.
+
+if script is being run as a script (not imported) call main() function
+
+"""
+
+import sys
+
+def function():
+    """
+    """
+    pass
+
+def main():
+    """
+    """
+    print('This program will do cool stuff.')
+
+    function()
+
+    print('\r\nFinished!')
+    sys.exit()
+
+if __name__ == '__main__':
+    main()
